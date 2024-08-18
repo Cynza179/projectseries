663e0e70e9e59085342fe688e997c80bbc9abba0
Cynza179 <cynza179@gmail.com>
2024-08-17 21:29:18 -0500

    second commit

--- validador_correlativos/settings.py
+++ validador_correlativos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['pycorrser.onrender.com', 'your-custom-domain.com', '127.0.0.1']
+ALLOWED_HOSTS = ['projectseries.onrender.com', 'your-custom-domain.com', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
